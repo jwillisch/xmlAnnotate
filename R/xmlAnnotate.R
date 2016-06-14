3a03edd4c6e17dd75c2b61514dcb2f281980e360
conjugateprior <conjugateprior@gmail.com>
2016-06-14 16:23:15 -0400

    Package passes all tests

---
+++ R/xmlAnnotate.R
@@ -0,0 +1,13 @@
+#' xmlAnnotate: A package for extracting annotations.
+#'
+#' the xmlAnnotate package contains three functions: \code{get_tagset} for extracting
+#' tagset information from an XML output file in a rather particular format,
+#' \code{get_tagsets} which applies this to lots of such files and concatenates the
+#' results, and \code{xml_grab}, which does, umm, something similar. You should probably
+#' ask Jakob about that.
+#'
+#' There's a vignette, showing the first two functions at work.
+#'
+#' @docType package
+#' @name xmlAnnotate
+NULL
